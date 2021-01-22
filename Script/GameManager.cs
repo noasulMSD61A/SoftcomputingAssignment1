ab00fb803b60f1d6b5ffebbfaebafe6458c9f828
noasulMSD61A <56297790+noasulMSD61A@users.noreply.github.com>
2021-01-22 01:44:53 +0100

    KU2 part2

--- Script/GameManager.cs
+++ Script/GameManager.cs
@@ -6,14 +6,17 @@ public class GameManager : MonoBehaviour
 {
     
     GameObject AI;
+    GameObject Obstacles;
     void Start()
     {
-        
-
         AI = Resources.Load("AI") as GameObject;
-        
-        Instantiate(AI, new Vector3(5,5,0), Quaternion.identity);
-        
+        Instantiate(AI, new Vector3(-40,40,0), Quaternion.identity);
+
+        Obstacles = Resources.Load("Obstacle") as GameObject;
+        for (int i = 0; i <= 4; i++)
+        {
+            Instantiate(Obstacles, new Vector3(Random.Range(-48f, 48f), Random.Range(-48f, 48f), 0), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
--- Script/customMove.cs
+++ Script/customMove.cs
@@ -12,8 +12,9 @@ public class customMove : MonoBehaviour
 
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Target").transform;//storing enemy position
         AstarPath.active.Scan();//scanning the grid graph
+        StartCoroutine(updateGraph());
+        target = GameObject.FindGameObjectWithTag("Target").transform;//storing enemy position
         seeker = GetComponent<Seeker>();//this will find our path towards the enemy
         pathToFollow = seeker.StartPath(transform.position, target.position);//setting the path between our AI position and the enemy position
         StartCoroutine(moveTowards(this.transform));//this coroutine will make our player/AI move to the enemy position
@@ -24,16 +25,28 @@ public class customMove : MonoBehaviour
         while (true) //creation of infinte loop
         {
             List<Vector3> positns = pathToFollow.vectorPath;//creating a list of positions that converts our path to avector form
-            for (int counter = 0; counter < positns.Count; counter++)//this loop is traversing for every posision with counter
+            for (int count = 0; count < positns.Count; count++)//this loop is traversing for every posision with counter
             {
-                while (Vector3.Distance(pos.position, positns[counter]) >= 0.5f)//while the distance of the enemy from the position of the ai is more than 0.5f
+                while (Vector3.Distance(pos.position, positns[count]) >= 1f)//while the distance of the enemy from the position of the ai is more than 0.5f
                 {                                                            
-                    pos.position = Vector3.MoveTowards(pos.position, positns[counter], 2f);//move the position of the ai to the position of the enemy
+                    pos.position = Vector3.MoveTowards(pos.position, positns[count], 1f);//move the position of the ai to the position of the enemy
                     yield return new WaitForSeconds(0.2f);//wait 
                     pathToFollow = seeker.StartPath(pos.position, target.position);//making sure that the ai will not follow the path back when its done by removing the path
                     yield return seeker.IsDone();//making sure that the attempt to find it is done when it is on the enemy pos
                 }
             }
+            yield return null;
+        }
+    }
+
+    //this co routine will run in the start method to update the graph if any obstacles are in the way so that the ai finds its way arounf them.
+    //also since we are putting the obstacles on an obstacle layer and we are setting the obtacle layer mask to obstacle as well the ai will mark those obstacles
+    //as a non walkable path, therefore the ai will avoid them.
+    IEnumerator updateGraph()
+    {
+        while (true)
+        {
+            AstarPath.active.Scan();
             yield return null;
         }
     }
